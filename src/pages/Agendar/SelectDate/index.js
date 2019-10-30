1689b2157f74fb81c86b82bc67da4fbdccafaab2
laronir <laronir@gmail.com>
2019-10-30 19:32:58 -0300

    Selecionar horario + redirecionamento

--- src/pages/Agendar/SelectDate/index.js
+++ src/pages/Agendar/SelectDate/index.js
@@ -1,17 +1,63 @@
-import React, { useState } from 'react';
-import { TouchableOpacity } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { TouchableOpacity, Alert } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import Background from '~/components/Background';
 import DateInput from '~/components/DateInput';
-import { Container } from './styles';
+import api from '~/services/api';
 
-const SelectDate = () => {
+import { Container, Hour, HourList, Title } from './styles';
+
+const SelectDate = ({ navigation }) => {
   const [date, setDate] = useState(new Date());
+  const [time, setTime] = useState([]);
+
+  const provider = navigation.getParam('provider');
+
+  useEffect(() => {
+    const checkAvailability = async () => {
+      try {
+        const res = await api.get(`providers/${provider.id}/available`, {
+          params: {
+            date: date.getTime(),
+          },
+        });
+        setTime(res.data);
+      } catch (err) {
+        Alert.alert(
+          'Erro',
+          'Erro ao buscar horários disponíveis. Verifique a sua conexão!'
+        );
+      }
+    };
+    checkAvailability();
+  }, [date, provider.id]);
+
+  const handleSelectHour = data => {
+    navigation.navigate('Confirm', {
+      provider,
+      time: data,
+    });
+  };
 
   return (
     <Background>
       <Container>
         <DateInput date={date} onChange={setDate} />
+        <HourList
+          data={time}
+          keyExtractor={item => item.time}
+          renderItem={({ item }) => (
+            <Hour
+              onPress={() => {
+                handleSelectHour(item.value);
+              }}
+              enabled={item.available}
+            >
+              <Title>{item.time}</Title>
+            </Hour>
+          )}
+        />
       </Container>
     </Background>
   );
@@ -28,3 +74,10 @@ SelectDate.navigationOptions = ({ navigation }) => ({
     </TouchableOpacity>
   ),
 });
+
+SelectDate.propTypes = {
+  navigation: PropTypes.shape({
+    getParam: PropTypes.func,
+    navigate: PropTypes.func,
+  }).isRequired,
+};
--- src/pages/Agendar/SelectDate/styles.js
+++ src/pages/Agendar/SelectDate/styles.js
@@ -1,7 +1,31 @@
 import styled from 'styled-components/native';
 import { Platform } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
 
 export const Container = styled.SafeAreaView`
   flex: 1;
   margin-top: ${Platform.OS === 'ios' ? '10px;' : '50px;'};
+`;
+
+export const HourList = styled.FlatList.attrs({
+  numColumns: 2,
+  showsVericalScrollIndicator: false,
+})`
+  padding: 0 20px;
+`;
+
+export const Hour = styled(RectButton)`
+  background: #fff;
+  border-radius: 6px;
+  padding: 20px;
+  flex: 1;
+  ${props => !props.enabled && 'opacity: 0.6;'};
+  align-items: center;
+  margin: 0 10px 20px;
+`;
+
+export const Title = styled.Text`
+  font-size: 14px;
+  font-weight: bold;
+  color: #333;
 `;
